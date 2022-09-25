4e60f12c5ea750d16b0346ac142083ba56d5fcd7
brymher <brymher@gmail.com>
2022-09-25 11:46:58 +0300

    Added Desktop Samples

--- samples/core-jvm/desktop/build.gradle.kts
+++ samples/core-jvm/desktop/build.gradle.kts
@@ -11,8 +11,24 @@ kotlin {
     sourceSets {
         val desktopMain by getting {
             dependencies {
+                implementation(compose.runtime)
+                implementation(compose.material)
+                implementation(compose.desktop.currentOs)
                 implementation(project(":narrator:core-jvm"))
+                implementation(project(":samples:core-jvm:common"))
             }
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "libetal.kotlin.compose.narrator.desktop.MainKt"
+        nativeDistributions {
+            targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
+            packageName = "ITrack"
+            packageVersion = "1.0.0"
         }
     }
 }
